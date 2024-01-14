e86d7b86b4aac75e521564646f3a20a12abb8994
zthanhduong <155933127+zthanhduong@users.noreply.github.com>
2024-01-14 20:55:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,17 +3,17 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
-        "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日了哦",
-        "去年的生日",
-        "还记得在王婆串串吃的呢",
-        "今年要吃好的喽哦",
-        "要把我家可爱猪猪喂饱饱",
-        "然后抱走",
-        "YAMI~~",
+        "To: Hara",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "Mừng sinh nhật tuổi 24 của bạn.",  // 同上...
+        "Mong bạn có một ngày thật vui, và cả cho tuổi 24 nữa.",
+        "Thật tiếc vì đã không thể gặp bạn hôm nay,",
+        "nhưng mình vẫn vui vì đã được đồng hành trong quá trình trưởng thành của bạn.",
+        "Những món quá nhỏ nhỏ trong hộp,",
+        "mình cũng không dám chắc nó đẹp hay phù hợp với bạn không.",
+        "Nhưng mình mong bạn luôn giữ ấm được cơ thể,",
+        "nên hãy coi đó như một lời nhắc mỗi khi thấy nó nhé.",
+        "Mong sớm được gặp lại bạn!",
+        "Your Soulmate: zthanhduong~~",
     ],
    /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -25,7 +25,7 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "Một bức ảnh dễ thương!": "./imgs/xiaokeai.jpeg",
+        "Một bức ảnh dễ thương...!": "./imgs/xiaokeai.jpeg",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
